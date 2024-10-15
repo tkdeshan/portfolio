0743ad2bfc755a67351e8396956eb84aff2c5953
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2024-10-15 22:46:05 +0530

    make portfolio structure

---
+++ constants/projects.js
@@ -0,0 +1,28 @@
+const projects = [
+  {
+    id: 1,
+    filter: "filter-iot",
+    category: "IoT",
+    title: "Smart Home",
+    image: "assets/img/projects/Smart Home.jpg",
+    slides: [
+      "assets/img/projects/Smart Home.jpg",
+      "assets/img/blog/mern.png",
+      "assets/img/projects/Smart Home.jpg",
+    ],
+    date: "20204/10/02",
+    context: "Acede",
+    client: "asd",
+    contributor_1_name: "Shammi",
+    contributor_1_linkedin: "https://github.com/tkdeshan/Smart_Home/",
+    contributor_2_name: "efffff",
+    contributor_2_linkedin: "",
+    contributor_3_name: "fefe",
+    contributor_3_linkedin: "",
+    demonstration: "https://github.com/tkdeshan/Smart_Home/",
+    weburl: "https://github.com/tkdeshan/Smart_Home/",
+    github: "https://github.com/tkdeshan/Smart_Home/",
+    description:
+      "This is the prototype of a smart home system that can securely control lights, switches, gates, and doors using a mobile phone. It utilizes sensors to detect objects and flames. The security status, sensor states, and other controlling element states can be viewed on the mobile phone.",
+  },
+];
